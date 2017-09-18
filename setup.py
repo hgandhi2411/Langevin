698988273b5f73f595ae6d389554a826dd4a1c66
hgandhi2411 <hgandhi@ur.rochester.edu>
2017-09-18 11:43:23 -0400

    Made package installable

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Langevin',
+      version='0.1',
+      description='Langevin Simulator',
+      author='Heta Gandhi',
+      author_email='hgandhi@ur.rochester.edu',
+      packages=['Lans'],
+     )
