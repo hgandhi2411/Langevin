afcb6968eb4ffa9253ba3edf5e3b47db1ee6f68d
hgandhi2411 <hgandhi@ur.rochester.edu>
2017-09-20 11:35:28 -0400

    Made more documentation

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup
 
+with open('Redme.md') as f:
+      long_description = ''.join(f.readlines())
+
 setup(name='Langevin',
       version='0.1',
       description='Langevin Simulator',
@@ -7,3 +10,4 @@ setup(name='Langevin',
       author_email='hgandhi@ur.rochester.edu',
       packages=['Lans'],
      )
+     
