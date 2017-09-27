77ca6231cc2f969a272c9114ceacfcd08a3768c9
hgandhi2411 <hgandhi@ur.rochester.edu>
2017-09-27 09:53:31 -0400

    File read correctly

--- Lans/lans.py
+++ Lans/lans.py
@@ -1,2 +1,6 @@
+import numpy as np
+
 def read_energy(input_file):
-    return None, None
+    data = np.genfromtxt(input_file, delimiter = ' ', skip_header=1)
+    data = np.transpose(data)
+    return data[0], data[1]
--- tests/test_lans.py
+++ tests/test_lans.py
@@ -3,20 +3,9 @@ import io
 import numpy as np
 
 def test_read_energy_read():
-    '''TEsts if the function reads and returns for given input file'''
+    '''TEsts if the function reads and returns correctly for given input file'''
 
-    test_string = '''
-    #test input enegry
-    #x energy
-    0   -2
-    1   -1
-    2    0
-    3    0
-    4    3
-    '''
-    #''' starts literal mode
+    pos, energy = Lans.read_energy(r"C:\Users\hetag\Desktop\CHE477\Langevin\testfile.txt")
+    assert(np.isclose(pos, [0,1,2,3,4])).all()
+    assert(np.isclose(energy, [-2, -1, 0, 0, 3])).all()
 
-    test_file = io.StringIO(test_string)
-    pos, energy = Lans.read_energy(test_file)
-    assert(np.isclose(pos, [0,1,2,3,4]))
-    assert(np.isclose(energy, [-2, -1, 0, 0, 3]))
